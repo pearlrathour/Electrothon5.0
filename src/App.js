95240905c59e097a98bca03d8a33bc864e35799b
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-09-06 19:13:21 +0530

    Dir Setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-3xl">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
---
+++ src/Sections/milestones.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function milestones() {
+  return (
+    <div>milestones</div>
+  )
+}
