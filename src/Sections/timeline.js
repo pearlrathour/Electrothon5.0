cb456607121e4a259137274994932d8953c9f69f
Pearl Rathour <pearl.rathour33@gmail.com>
2022-10-11 23:46:03 +0530

    Timeline

--- src/Sections/timeline.js
+++ src/Sections/timeline.js
@@ -2,87 +2,105 @@ import React from "react";
 
 export default function timeline() {
   return (
-    <div className="w-full bg-black text-white text-center">
-      <div className="container bg-black mx-auto w[2000px] h[2000px]">
-        <br />
-        <h1>Timeline</h1>
-        <br />
-        <div className=" relative wrap overflow-hidden p-10 h-full w-full">
-          <div class="border-2-2 absolute border-opacity-20 border-white-700 h-full border"></div>
-          {/* / !-- right timeline --  */}
-          <div className=" hover:scale-105 duration-300 mb-8 flex justify-between items-center w-full right-timeline ">
-            <div className="order-1 w-5/12"></div>
-            <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-              <h1 className="mx-auto font-semibold text-lg text-white">1</h1>
-            </div>
-            <div className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-              <h3 className="mb-3 font-bold text-gray-800 text-xl">
-                Lorem Ipsum
-              </h3>
-              <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-              </p>
-            </div>
-          </div>
+    <div className="flex flex-col w-full px-10 lg:px-20 bg-black justify-center py-6 lg:py-12">
+      <h1 class="text-5xl font-normal leading-normal mt-0 mb-2 text-center text-white">
+        Timeline
+      </h1>
+      <div className="w-full mx-auto lg:max-w-4xl">
+        <div className="relative">
+          <div className="absolute hidden w-px h-full transform -translate-x-1/2 bg-indigo-300 lg:block left-1/2"></div>
+          <div className="space-y-12 lg:space-y-8 text-center">
 
-          {/* <!-- left timeline --> */}
-          <div className=" hover:scale-105 duration-300 mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-            <div className="order-1 w-5/12"></div>
-            <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-              <h1 className="mx-auto text-white font-semibold text-lg">2</h1>
+            <div>
+              <div className="flex flex-col items-center">
+                <div className="flex items-center justify-start w-full mx-auto">
+                  <div className="w-full lg:w-1/2 lg:pr-8">
+                    <div className="p-4 bg-white rounded shadow-lg shadow-cyan-200">
+                      <p className="font-extrabold">17 Dec 2022</p>
+                      <p>Registration Commences</p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="absolute flex items-center justify-center w-8 h-8 transform -translate-x-1/2 -translate-y-4 bg-sky-300 border-2 border-sky-200 rounded-full left-1/2 sm:translate-y-0">
+                  <span className="text-white">1</span>
+                </div>
+              </div>
             </div>
-            <div className="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-              <h3 clasName="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
-              <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-              </p>
-            </div>
-          </div>
 
-          {/* <!-- right timeline --> */}
-          <div className=" hover:scale-105 duration-300 mb-8 flex justify-between items-center w-full right-timeline">
-            <div className="order-1 w-5/12"></div>
-            <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-              <h1 className="mx-auto font-semibold text-lg text-white">3</h1>
-            </div>
-            <div className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-              <h3 className="mb-3 font-bold text-gray-800 text-xl">
-                Lorem Ipsum
-              </h3>
-              <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-              </p>
-            </div>
-          </div>
 
-          {/* <!-- left timeline --> */}
-          <div className=" hover:scale-105 duration-300 mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-            <div className="order-1 w-5/12"></div>
-            <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-              <h1 className="mx-auto text-white font-semibold text-lg">4</h1>
+            <div>
+              <div className="flex flex-col items-center">
+                <div className="flex items-center justify-end w-full mx-auto">
+                  <div className="w-full lg:w-1/2 lg:pl-8">
+                    <div className="p-4 bg-white rounded shadow-lg shadow-cyan-200">
+                      <p className="font-extrabold">30 Jan 2023</p>
+                      <p>Registration Closes</p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="absolute flex items-center justify-center w-8 h-8 transform -translate-x-1/2 -translate-y-4 bg-sky-300 border-2 border-sky-200 rounded-full left-1/2 sm:translate-y-0">
+                  <span className="text-white">2</span>
+                </div>
+              </div>
             </div>
-            <div className="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-              <h3 className="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
-              <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-              </p>
+
+
+            <div>
+              <div className="flex flex-col items-center">
+                <div className="flex items-center justify-start w-full mx-auto">
+                  <div className="w-full lg:w-1/2 lg:pr-8">
+                    <div className="p-4 bg-white rounded shadow-lg shadow-cyan-200">
+                      <p className="font-extrabold">10 Feb 2023</p>
+                      <p>Proposal Submission</p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="absolute flex items-center justify-center w-8 h-8 transform -translate-x-1/2 -translate-y-4 bg-sky-300 border-2 border-sky-200 rounded-full left-1/2 sm:translate-y-0">
+                  <span className="text-white">3</span>
+                </div>
+              </div>
             </div>
+
+
+            <div>
+              <div className="flex flex-col items-center">
+                <div className="flex items-center justify-end w-full mx-auto">
+                  <div className="w-full lg:w-1/2 lg:pl-8">
+                    <div className="p-4 bg-white rounded shadow-lg shadow-cyan-200">
+                      <p className="font-extrabold">12 Feb 2023</p>
+                      <p>Proposal Shortlisting</p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="absolute flex items-center justify-center w-8 h-8 transform -translate-x-1/2 -translate-y-4 bg-sky-300 border-2 border-sky-200 rounded-full left-1/2 sm:translate-y-0">
+                  <span className="text-white">4</span>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex flex-col items-center">
+                <div className="flex items-center justify-start w-full mx-auto">
+                  <div className="w-full lg:w-1/2 lg:pr-8">
+                    <div className="p-4 bg-white rounded shadow-lg shadow-cyan-200">
+                      <p className="font-extrabold">19 Feb 2023</p>
+                      <p>Hack Day Electrothon 5.0</p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="absolute flex items-center justify-center w-8 h-8 transform -translate-x-1/2 -translate-y-4 bg-sky-300 border-2 border-sky-200 rounded-full left-1/2 sm:translate-y-0">
+                  <span className="text-white">5</span>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
-        <br />
-        <br />
       </div>
     </div>
   );
